08e874de5f6767422622510dcb7476d9103cab7f
Marina Ilyeva <marinka873@gmail.com>
2022-12-05 21:19:52 +0300

    File system (FS)

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,13 @@
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+
 const create = async () => {
-    // Write your code here 
+    fs.writeFile(
+        path.join( './src/fs/files', 'fresh.txt'),
+         'I am fresh and young', (error) => {
+       if (error) throw error
+      console.log(error,'FS operation failed');
+    })
 };
 
 await create();
--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { readFileSync } from 'node:fs';
+import {
+    createReadStream
+  } from 'node:fs';
+const {
+    createHash
+  } = await import('node:crypto');
+
+
+
 const calculateHash = async () => {
-    // Write your code here 
+
+    const file = readFileSync('../../files/filToCalculateHashFor.txt')
+    const hash = createHash('sha256')
+    hash.update(file)
+
+    const fileToHex = hash.digest('hex')
+    console.log(fileToHex);
+
 };
 
 await calculateHash();
